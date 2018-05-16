f5cfcc6b9c3f7cde082f6540e3d4a336617cd18b
Ryuji Ganaha <ryujignh@gmail.com>
2018-05-16 09:21:31 +0900

    Adding new and create functions to Category controller [ci skip]

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,4 +5,23 @@ class CategoriesController < ApplicationController
     @items = @category.items
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to(root_path)
+    else
+      render_action(:new)
+      flash[:error] = @category.errors.full_messages
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name, :name_en, :image)
+  end
+
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
 
-  def test_index
+  def test_show
     category = categories(:eat)
     get category_path(category)
     assert_response(:success)
@@ -14,6 +14,30 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
       "Should define @items belongs to #{category}")
     assert(assigns[:items].first == items(:item_eat),
       "Should define @items belongs to #{category}")
+  end
+
+  def test_new
+    get new_category_path
+    assert_response(:success)
+    assert(assigns[:category],
+      "Should define new category object")
+  end
+
+  def test_create
+    post categories_path(create_params)
+    assert_redirected_to(root_path)
+  end
+
+  private
+
+  def create_params
+    {
+      category: {
+        name: "新カテゴリ",
+        name_en: "New category",
+        image: "",
+      }
+    }
   end
 
 end
