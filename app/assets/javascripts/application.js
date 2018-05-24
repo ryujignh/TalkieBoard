ab85a8fc7998b38a6873deb9a0f12b4cadb8f313
Ryuji Ganaha <ryujignh@gmail.com>
2018-05-24 09:13:45 +0900

    Removed foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require foundation
 //= require_tree .
-$(document).on('turbolinks:load', function() {
-  $(function(){ $(document).foundation(); });
-});
